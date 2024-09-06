e31ae66b679368237e98e9fff269a62653a324de
Peter Atkinson <peterjatkinson@gmail.com>
2024-09-06 17:01:40 +0100

    Updated all content

--- src/components/AdvertisingTimeline.js
+++ src/components/AdvertisingTimeline.js
@@ -5,75 +5,77 @@ import { faPenNib, faNetworkWired, faGlobe, faTv, faTag, faCamera, faRobot } fro
 
 //Images
 import img1960s from '../images/1960sad.webp';
+import img1990s from '../images/1990sad.avif';
+import img2000s from '../images/2000sad.jpg';
+import img2010s from '../images/2010sad.png';
 
 
 const timelineData = [
     {
         decade: '1960s',
         title: 'The birth of the creative revolution',
-        description: 'The rise of creative-driven advertising, focusing on wit and storytelling.',
-        keyEvent: 'Madison Avenue agencies changed the landscape by prioritizing creativity over hard-selling tactics.',
-        iconicAd: 'Volkswagen\'s "Think Small" (1960)',
-        impact: 'This minimalist ad revolutionized car advertising by emphasizing simplicity and irony.',
+        description: 'The rise of creative-driven advertising, focusing on wit and storytelling. Madison Avenue agencies changed the landscape by prioritising creativity over hard-selling tactics.',
+        exampleAd: 'Volkswagen\'s \'Think Small\' (1960)',
+        impact: 'This minimalist ad revolutionised car advertising by emphasising simplicity and irony.',
         icon: () => <FontAwesomeIcon icon={faPenNib} />,
         image: img1960s
     },
     {
         decade: '1970s',
         title: 'The power of branding',
-        description: 'A shift from selling products to building brand identities.',
-        keyEvent: 'Brands began focusing on emotional connections and lifestyle imagery.',
-        iconicAd: 'Coca-Cola\'s "I\'d Like to Buy the World a Coke" (1971)',
+        description: 'A shift from selling products to building brand identities. Brands began focusing on emotional connections and lifestyle imagery.',
+        exampleAd: 'Coca-Cola\'s \'I\'d Like to Buy the World a Coke\' (1971)',
         impact: 'This ad showcased a message of unity and peace, associating Coca-Cola with positive, uplifting emotions.',
         icon: () => <FontAwesomeIcon icon={faTag} />,
         youtubeVideo: 'https://www.youtube.com/embed/ib-Qiyklq-Q'  // YouTube video link
     },
     {
         decade: '1980s',
-        title: 'Big budget, big ideas, and the rise of tv commercials',
-        description: 'Television became the dominant medium for advertising.',
-        keyEvent: 'Increased ad budgets led to more dramatic, story-driven TV commercials.',
-        iconicAd: 'Apple\'s "1984" Super Bowl Ad (1984)',
+        title: 'Big budget, big ideas and the rise of tv commercials',
+        description: 'Television became the dominant medium for advertising. Increased ad budgets led to more dramatic, story-driven TV commercials.',
+        exampleAd: 'Apple\'s \'1984\' Super Bowl Ad (1984)',
         impact: 'Introduced the Macintosh computer and portrayed Apple as a rebel challenging the status quo.',
         icon: () => <FontAwesomeIcon icon={faTv} />,
+        youtubeVideo: 'https://www.youtube.com/embed/VtvjbmoDx-I'  // Apple 1984 ad
     },
     {
         decade: '1990s',
-        title: 'Digital beginnings and shock advertising',
-        description: 'The rise of early digital ads and controversial "shock" advertising.',
-        keyEvent: 'The beginning of digital advertising with the rise of the internet.',
-        iconicAd: 'Benetton\'s "Unhate" Campaign',
-        impact: 'Benetton became known for its shocking ads that dealt with social issues, aiming to provoke conversations.',
+        title: 'Digital beginnings in advertising',
+        description: 'The rise of early digital ads with the emergence of the internet. The first digital banner ad launched by AT&T in 1994 on HotWired.',
+        exampleAd: 'AT&T’s first banner ad (1994)',
+        impact: 'This banner ad marked the birth of online advertising, creating a foundation for future digital marketing.',
         icon: () => <FontAwesomeIcon icon={faGlobe} />,
+        image: img1990s
     },
     {
         decade: '2000s',
         title: 'The rise of digital and guerrilla marketing',
-        description: 'The explosion of digital marketing and guerrilla tactics.',
-        keyEvent: 'Brands increasingly invested in online campaigns, viral marketing, and guerrilla strategies.',
-        iconicAd: 'Burger King\'s "Subservient Chicken" (2004)',
-        impact: 'This online campaign allowed users to "control" a chicken via a website, pioneering interactive and viral marketing.',
+        description: 'The explosion of digital marketing and guerrilla tactics. Brands increasingly invested in online campaigns, viral marketing and guerrilla strategies.',
+        exampleAd: 'Burger King\'s \'Subservient Chicken\' (2004)',
+        impact: 'This online campaign allowed users to \'control\' a chicken via a website, pioneering interactive and viral marketing.',
         icon: () => <FontAwesomeIcon icon={faNetworkWired} />,
+        image: img2000s
     },
     {
         decade: '2010s',
-        title: 'Social media, influencers, and the age of authenticity',
-        description: 'The dominance of social media and the rise of influencer marketing.',
-        keyEvent: 'Brands started shifting budgets to social media platforms, using influencers to authentically connect with consumers.',
-        iconicAd: 'Always\' "Like a Girl" Campaign (2014)',
-        impact: 'This ad challenged gender stereotypes and empowered women, sparking a cultural conversation.',
+        title: 'The rise of influencer marketing',
+        description: 'The dominance of social media and influencers as brands shifted their marketing budgets to platforms like Instagram.',
+        exampleAd: 'Daniel Wellington Instagram Influencer Campaign',
+        impact: 'Daniel Wellington became a major watch brand by giving influencers free watches in exchange for social media posts, setting a standard for influencer-driven marketing.',
         icon: () => <FontAwesomeIcon icon={faCamera} />,
-    },
+        image: img2010s
+        },
     {
         decade: '2020s',
-        title: 'Personalization, AI, and the Power of Data',
+        title: 'Personalisation, AI and the power of data',
         description: 'The rise of AI-driven, data-fueled personalized marketing.',
-        keyEvent: 'Brands increasingly used data and AI to create hyper-targeted, personalized ads.',
-        iconicAd: 'Spotify\'s "Wrapped" Campaign',
-        impact: 'Spotify\'s yearly "Wrapped" campaign gave users a personalized summary of their listening habits, transforming private data into a viral phenomenon.',
+        exampleAd: 'Coca-Cola \'Create Real Magic\' AI-powered campaign (2023)',
+        impact: 'Coca-Cola’s use of AI in this campaign has redefined how brands interact with consumers, encouraging them to create personalized digital art with Coca-Cola’s assets using advanced AI tools.',
         icon: () => <FontAwesomeIcon icon={faRobot} />,
+        youtubeVideo: 'https://www.youtube.com/embed/YDGknvJ55Ag'  // Coca-Cola Real Magic AI Campaign video (embed link)
     },
 ];
+
 
 const ImageModal = ({ isOpen, onClose, imageSrc, alt }) => {
     if (!isOpen) return null;
@@ -163,12 +165,8 @@ const AdvertisingTimeline = () => {
                             <h3 className="text-2xl font-semibold text-gray-700">{currentEra.title}</h3>
                             <p className="text-gray-600">{currentEra.description}</p>
                             <div>
-                                <h4 className="text-xl font-semibold text-gray-700 mb-2">Key event:</h4>
-                                <p className="text-gray-600">{currentEra.keyEvent}</p>
-                            </div>
-                            <div>
-                                <h4 className="text-xl font-semibold text-gray-700 mb-2">Iconic ad:</h4>
-                                <p className="text-gray-600">{currentEra.iconicAd}</p>
+                                <h4 className="text-xl font-semibold text-gray-700 mb-2">Example ad:</h4>
+                                <p className="text-gray-600">{currentEra.exampleAd}</p>
                             </div>
                             <div>
                                 <h4 className="text-xl font-semibold text-gray-700 mb-2">Impact:</h4>
@@ -184,7 +182,7 @@ const AdvertisingTimeline = () => {
                                     width="400"
                                     height="225"
                                     src={currentEra.youtubeVideo}
-                                    title={`Iconic ad for the ${currentEra.decade}`}
+                                    title={`Example ad for the ${currentEra.decade}`}
                                     frameBorder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                     allowFullScreen
