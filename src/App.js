d03eb1f2a47a09e483d974fef200a353d9d55afa
Peter Atkinson <peterjatkinson@gmail.com>
2024-10-03 11:37:23 +0100

    Added more accessibility features

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import AdvertisingTimeline from './components/AdvertisingTimeline';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" aria-label="Interactive advertising timeline: Explore the evolution of creative advertising through the decades">
       <AdvertisingTimeline />
     </div>
   );
 }
 
 export default App;
-
